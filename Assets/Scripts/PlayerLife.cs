ba3b45b64d54eab211bffc99258829ab6ce63e47
Alan Margairaz <apmargairaz@gmail.com>
2022-12-02 20:49:48 +0100

    Added PlayerLife script, not finished yet

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    //private Animator DeathAnim;
+
+    private void Start()
+    {
+        rb.GetComponent<Rigidbody2D>();
+        //DeathAnim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Spike"))
+        {
+            Die();
+        }
+    }
+    
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        //DeathAnim.SetTrigger("death")
+    }
+}
